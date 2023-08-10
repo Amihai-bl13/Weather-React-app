30009d060ca52f4ea486d391e2fd3079e87ec197
Amihai Blekherov <amihaibl@post.bgu.ac.il>
2023-08-10 11:26:46 +0300

    Making the website more friendly and UI responsive

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404)
+        return true;
+    else
+        return false;
+}
+
 let weather = {
     "apiKey": "b7009dd707ccd3607889bac244c06116",
     fetchWeather: function(city){
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="+city
-            + "&units=metric&appid=" + this.apiKey
-            ).then((response) => response.json()
-            ).then((data) => this.displayWeather(data));
+        url = "https://api.openweathermap.org/data/2.5/weather?q="+city
+        + "&units=metric&appid=" + this.apiKey;
+        UrlExists(url) ?  fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="+city 
+            + "&units=metric&appid=" + this.apiKey).then((response) => response.json())
+            .then((data) => this.displayWeather(data))
+            : alert("No such city, please make sure that you've typed it in correctly!");
         },
 
     displayWeather: function(data){
-        const {name} = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        //console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png"
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
-        document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+            const {name} = data;
+            const {icon, description} = data.weather[0];
+            const {temp, humidity} = data.main;
+            const {speed} = data.wind;
+            //console.log(name, icon, description, temp, humidity, speed);
+            document.querySelector(".city").innerText = "Weather in " + name;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +".png"
+            document.querySelector(".description").innerText = description;
+            document.querySelector(".temp").innerText = temp + "°C";
+            document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+            document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+            document.querySelector(".weather").classList.remove("loading");
+            document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
+
     search: function(){
-        this.fetchWeather(document.querySelector(".search-bar").value);
+            this.fetchWeather(document.querySelector(".search-bar").value);
     }
-};
+}
 
 document.querySelector(".search button")
 .addEventListener("click", function () {
